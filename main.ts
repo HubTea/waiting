6917d718d0bcda4765a14354ed9ad13ae6f04496
HubTea <k3392255@naver.com>
2025-05-26 20:53:00 +0900

    환경변수를 전역변수에 대입
    명시적으로 .env 파일의 스키마를 선언하는 효과를 보기 위해서
    환경변수를 전역변수에 대입하는 코드를 넣었다.

--- main.ts
+++ main.ts
@@ -2,7 +2,10 @@ import 'dotenv/config';
 import {Sequelize, Model, DataTypes} from 'sequelize';
 import express from 'express';
 
-let sequelize = new Sequelize(process.env.DB_URL as string);
+const DATABASE_URL: string = process.env.DATABASE_URL!;
+const LISTEN_PORT: number = parseInt(process.env.LISTEN_PORT!);
+
+let sequelize = new Sequelize(DATABASE_URL);
 sequelize.authenticate().then(() => console.log('sequelize authenticated'));
 
 interface WaitingEntity {
@@ -53,11 +56,11 @@ Waiting.init({
 });
 
 let app = express();
-app.listen(parseInt(process.env.LISTEN_PORT as string), function (error) {
+app.listen(LISTEN_PORT, function (error) {
     if(error) {
         throw error;
     }
-    console.log('listening ' + process.env.LISTEN_PORT)
+    console.log('listening ' + LISTEN_PORT)
 });
 
 app.post('/waiting', async function register(request, response) {
