c82612a1639c4a30b0eb2c085df8fcef5a145441
HubTea <k3392255@naver.com>
2025-05-27 16:50:38 +0900

    init 함수 수정
    테이블 내 데이터 삭제
    Singleton 모델의 추가 컬럼 초기화

--- src/service/init.ts
+++ src/service/init.ts
@@ -1,9 +1,13 @@
 import { CAPACITY } from "../config";
-import { createSingleton} from '../repository/singleton';
+import { Singleton, createSingleton} from '../repository/singleton';
+import { Waiting } from "../repository/waiting";
 
 export async function init() {
+    await Waiting.truncate();
+    await Singleton.truncate();
     await createSingleton({
         id: 0,
         capacity: CAPACITY,
+        lastNumber: 0,
     });
 }
