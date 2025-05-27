66c85f1c6c0b7375027a3a33c315a6eed2c2acca
HubTea <k3392255@naver.com>
2025-05-27 20:09:16 +0900

    시간 값의 단위를 주석으로 표시

--- src/config.ts
+++ src/config.ts
@@ -6,4 +6,5 @@ export const DATABASE_POOL_MAX: number = parseInt(process.env.DATABASE_POOL_MAX!
 export const LISTEN_PORT: number = parseInt(process.env.LISTEN_PORT!);
 
 export const CAPACITY: number = parseInt(process.env.CAPACITY!);
+//단위: ms
 export const AUTHORIZATION_REFRESH_TIME: number = parseInt(process.env.AUTHORIZATION_REFRESH_TIME!);
