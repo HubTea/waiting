3d0752cc0ae0def8026358b70d965bab73e16d27
HubTea <k3392255@naver.com>
2025-05-28 16:27:57 +0900

    sync가 테이블을 drop하도록 수정

--- src/sync.ts
+++ src/sync.ts
@@ -3,5 +3,7 @@ import './repository/waiting';
 import { sequelize } from './connection';
 
 export async function sync() {
-    await sequelize.sync();
+    await sequelize.sync({
+        force: true,
+    });
 }
